f9b1bfd66969aab86ddc0b285c8728350695fac5
Richard Steinmetz <richard@steinmetz.cloud>
2022-06-28 17:24:27 +0200

    Revert --legacy-peer-deps workaround

--- remote.sh
+++ remote.sh
@@ -164,7 +164,7 @@ install_enable_app() {
                 npm_cmd=dev
             fi
 
-            if ! npm i --no-audit --legacy-peer-deps || ! npm run "$npm_cmd" --if-present; then
+            if ! npm i --no-audit || ! npm run "$npm_cmd" --if-present; then
                 echo "Could not compile the $APPID app."
                 exit 1
             fi
